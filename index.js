b90cf6eedb6a295e2c9399b8ad953c8bef183298
Sibongiseni Dubazane <sibongiseni.dubazane@safetyio.com>
2026-05-08 10:17:36 +0200

    introudiced controllers

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import Product from './models/product.model.js'
+import productRoute from './routes/product.route.js';
 
 const app= express();
 const PORT= 3000;
 app.use(express.json())//middleware
 app.use(express.urlencoded({extended:false}))
 
-
+//routes
+app.use('/api/products',productRoute)
 
 
 app.get('/', (req,res)=>{
     res.send("Hello from node Api server");
 });
 
-app.get('/api/products', async (req,res)=>{
-    try{
-        const products= await Product.find({});
-        res.status(200).json(products)
 
-    }catch(err){
-        res.status(500).json({message: err.message})
-    }
-})
 
-app.get('/api/product/:id', async (req,res)=>{
+app.get('/api/products/:id', async (req,res)=>{
     try{
         const {id}=req.params;
         const product=await Product.findById(id);
@@ -35,51 +29,10 @@ app.get('/api/product/:id', async (req,res)=>{
 });
 
 
-app.post('/api/products',async (req,res)=>{
-   try{
-        const product =  await Product.create(req.body);
-        res.status(200).json(product);
-
-   }catch(err){
-    res.status(500).json({message:err.message})
-   }
-});
-
-//update product  api
-app.put('/api/product/:id', async (req,res)=>{
-    try{
-        const {id}= req.params;
-        const product= await Product.findByIdAndUpdate(id, req.body,{new:true});
-        if (!product){
-            return res.status(404).json({message:"Product not found"})
-        }
-        const updatedProduct= await Product.findById(id);
-        res.status(200).json(updatedProduct);
-
-    }catch(err){
-        res.status(500).json({message: err.message});
 
 
-    }
 
 
-});
-
-//delete product
-app.delete('/api/product/:id', async (req,res)=>{
-    try{
-        const {id}= req.params;
-        const product= await Product.findByIdAndDelete(id);
-
-        if (!product){
-            return res.status(404).json({message:'product not found'});
-        }
-        res.status(200).json({message:'Producct deleted successfuly'})
-
-    }catch(err){
-        res.status(500).json({message: err.message})
-    }
-})
 
 
 mongoose.connect("mongodb+srv://sbosh88dubazane_db_user:SBH55springs@expressdb.hehk9nu.mongodb.net/Node-API?appName=expressDB")
--- routes/product.route.js
+++ routes/product.route.js
@@ -1,1 +1,13 @@
-//1:11:26
+import express from 'express';
+import Product from '../models/product.model.js';
+const router = express.Router();
+import {getProducts,getProduct,createProduct,updateProduct,deleteProduct} from '../controllers/product.controller.js'
+
+//controller function
+router.get('/',getProducts);
+router.get('/:id',getProduct);
+router.post('/',createProduct);
+router.put('/:id',updateProduct);
+router.delete('/:id',deleteProduct);
+
+export default router;
